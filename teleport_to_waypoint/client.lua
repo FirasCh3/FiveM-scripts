5345ea9a05127f0e5ba08702075f13153b40f4b9
Firas <firaschebil61@gmail.com>
2024-09-01 12:35:41 +0100

    changes

--- teleport_to_waypoint/client.lua
+++
@@ -1,5 +0,0 @@
-RegisterCommand("teleport", function()
-    local waypoint = GetFirstBlipInfoId(8)
-    StartPlayerTeleport(PlayerId(), GetBlipInfoIdCoord(waypoint).x, GetBlipInfoIdCoord(waypoint).y,
-        GetBlipInfoIdCoord(waypoint).z, 0.0, true, true, true)
-end)
--- weapon_spawner/client.lua
+++
@@ -1,3 +0,0 @@
-RegisterCommand("weapon", function()
-    GiveWeaponToPed(PlayerPedId(), 487013001, 1000, false, false)
-end)
